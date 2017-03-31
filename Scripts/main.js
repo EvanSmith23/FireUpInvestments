77a7fefb22e30077ce1d0f3aee6a99329f4c7d11
Evan Smith <smieva@umich.edu>
2017-03-30 22:56:38 -0400

    added small jquery UI features

---
+++ Scripts/main.js
@@ -0,0 +1,17 @@
+/*var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+// Multiply function
+function multiply(num1,num2) {
+  var result = num1 * num2;
+  return result;
+}*/
+
+$(document).ready( function() {
+	$("#Hermitage").slideUp(500).delay(2500).slideDown(500);
+	$("#Parkwood").slideUp(750).delay(1500).slideDown(750);
+	$("#Union").slideUp(1000).delay(800).slideDown(1000);
+	$("#footer").slideUp(1000).delay(1800).slideDown(1000);
+});
+
+
