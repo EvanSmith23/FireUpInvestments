405e70e2e8ce4dd4ed8f16426a0066a920c75d67
Evan Smith <smieva@umich.edu>
2017-05-08 13:00:31 -0400

    Update config.py

--- config.py
+++ config.py
@@ -6,13 +6,20 @@ from werkzeug import secure_filename
 import glob
 
 def connect_to_database():
-    options = {
+    
+    # creating arguments for connect function
+    options = 
+    {
     'host': '0.0.0.0',
     'user': 'root',
     'passwd': 'new',
     'db': 'FireUp',
     'cursorclass' : MySQLdb.cursors.DictCursor
     }
+    
+    #connecting to database with parameters passed in
     db = MySQLdb.connect(**options)
     db.autocommit(True)
+    
+    #returns a connection object from MYSQLDB
     return db
